38937f4078112dabcbddb804aadd9932a716d02b
horsemann07 <raghavjha1531@gmail.com>
2025-09-22 01:06:54 +0530

    fix: rearrange the folder structures and source files.

--- app/main/app_main.c
+++ app/main/app_main.c
@@ -4,16 +4,19 @@
 #include "esp_log.h"
 
 
-static const char *TAG = "CMSIS_DEMO";
+static const char* TAG = "CMSIS_DEMO";
 
-void MyCmsisTask(void *argument) {
-    while (1) {
+void MyCmsisTask(void* argument)
+{
+    while(1)
+    {
         ESP_LOGI(TAG, "Hello from CMSIS-RTOS2 task!");
-        osDelay(1000);   // CMSIS API → calls vTaskDelay
+        osDelay(1000); // CMSIS API → calls vTaskDelay
     }
 }
 
-void app_main(void) {
+void app_main(void)
+{
     // Initialize CMSIS-RTOS v2 kernel wrapper
     osKernelInitialize();
 
